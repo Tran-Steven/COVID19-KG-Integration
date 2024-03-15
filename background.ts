c2446b55e45556afb48e45a1e53bce56fa489607
Steven Tran <steven29160@gmail.com>
2024-03-15 02:32:46 -0400

    Delete background.ts

--- background.ts
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("https://chat.openai.com")) {
-    const queryParameters = new URLSearchParams(tab.url.split("?")[1]);
-    const urlParameters = new URLSearchParams(queryParameters);
-    
-  }
-});
